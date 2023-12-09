e29284f5d255f138d03512f8dfbb1cda3d3d9313
Reno_Home_ZepM16 <renoxiao93@gmail.com>
2023-12-09 22:04:08 +0800

    Revert "Revert "Basic Slash""
    This reverts commit ff32469936d0b6d62093c4b0447196efdd264779.

--- Assets/Effects_Inheritance/EffectsCreater.cs
+++ Assets/Effects_Inheritance/EffectsCreater.cs
@@ -2,34 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class EffectsCreater :MonoBehaviour
+public abstract class EffectsCreater :MonoBehaviour
 {
-    //基础信息
+    [Header("Basic Info")]
     public string effectName;
     public int effectID;
+    public GameObject effectHolder;
+    public GameObject effectTarget;
+    public GameObject prefabIns;
 
-    //使用信息
-    public GameObject effectUser;//表示谁使用这个效果，这个效果要给谁使用
-    public string effectReciever;//如果对方的tag满足接收者需要传递效果。比如碰到了tag是enemy，那么就要给enemy接收器发subeffects
-    public int effectStartTimePoint;//效果开始作用的时间点，比如是1代表Slash前2代表Slash时3代表Slash完成落地
-    public List<ScriptableObject> subEffects= new List<ScriptableObject>();//用于携带别的特效,例如该特效是生成collider,这个碰撞体还需要带一个伤害效果
+    //public List<effectletters> effectmessletters = new List<effectletters>();
+    //public class effectletters
+    //{
+    //    private string effectType;
+    //    private int damage;
+    //    public string EffectType
+    //    {
+    //        get { return effectType; }
+    //    }
+    //    public int Damage
+    //    {
+    //        get { return damage; }
+    //    }
+    //    public effectletters(string EffectType,int Damage)
+    //    {
+    //        this.effectType = EffectType;
+    //        this.damage = Damage;
+    //    }
+    //}
 
-
-    //行为接口
-    public interface Effect3C//这个是接口，用于处理效果的3C应该如何通过手指发生
+    //effect act on info
+    public enum HolderState
     {
-        void effecctInteractive();//这个是接口下面的接口函数
-    }
+        StateIdle,
+        Moving,
+    };
+    public enum CardState
+    {
+        CardReleasedTiming,
+        CardActivating,
+        CardEndTiming
+        //might more state like when card is drawed
+    };
+    public enum DamageTag
+    {
+        Damage,
+        Break,
+    };
 
-    public interface EffectIndicator//这个是接口，用于处理效果的范围和行为指示，比如显性的Slash判定框
+    //virtual function
+    public virtual void effectHolderDie()
     {
-        void effectSilhouette();//要注意不是每个卡牌都会带有有提示框的释放体验
+        Destroy(this.gameObject);
     }
-    public interface EffectBehavior//【使用出】卡牌后对象需要做什么
-    {
-        void effectUserDo();//使用者当卡牌用出后需要做什么
-        void subEffectDeliver();//如果触发了传递效果的条件（比如切到敌人了）怎么传递subeffects
-    }
+    //control
+    public abstract void cardInteraction();
+
+    //abstract function
+    public abstract void prefabInstantiater();
+    public abstract void C_NewcolliderInstantiater();//used to create a special collider instead of the original one
+    public abstract void AttackDamege();
+
+    public abstract void EffectMessenger(string Dtype, int Dnum);
 
 }
---
+++ Assets/MouseInteraction_Script.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class MouseInteraction_Script : MonoBehaviour
+{
+    [Header("REFs")]
+    public GameObject aimArea;
+    public GameObject slashPath;
+    public GameObject imageSlashPath; // first slash path, used to detect if slashable
+    public GameObject player;
+    // MOUSE DATAs
+    Vector3 mouseDown_pos;
+    Vector3 mouseCurrent_pos;
+    public Vector3 mouseDir;
+    [HideInInspector]
+    public float mouseDrag_time;
+    public float mouseDrag_maxTime;
+    public bool dragging = false;
+    #region SINGLETON
+    public static MouseInteraction_Script me;
+    private void Awake()
+    {
+        me = this;
+    }
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        mouseCurrent_pos = new();
+        dragging = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouse3Dposition();
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseDown_pos = mouseCurrent_pos; //Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        //mouseCurrent_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //mouseCurrent_pos.z = 0;
+        mouseDir = (mouseCurrent_pos - mouseDown_pos).normalized;
+        if (!Input.GetMouseButton(0))
+        {
+            mouseDir = Vector3.zero;
+        }
+        //print(mouseDir);
+    }
+
+    private void mouse3Dposition()
+    {
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 mousePosOnScreen = Input.mousePosition;
+        mousePosOnScreen.z = screenPos.z;
+        mouseCurrent_pos = Camera.main.ScreenToWorldPoint(mousePosOnScreen);
+        mouseCurrent_pos.y = 0;
+    }
+
+}
